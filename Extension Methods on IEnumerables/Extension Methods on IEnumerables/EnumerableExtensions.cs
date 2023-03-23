722ef8b6cdeb1712eeb075c8c9de8ec181b37711
NEERAJ DHAKAD <dhakadneeraj786@gmail.com>
2023-03-23 11:40:54 +0530

    Add files via upload

---
+++ Extension Methods on IEnumerables/Extension Methods on IEnumerables/EnumerableExtensions.cs
@@ -0,0 +1,59 @@
+﻿namespace Extension_Methods_on_IEnumerables
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class EnumerableExtensions
+    {
+        public static bool CustomAll<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (!predicate(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool CustomAny<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static T CustomMax<T>(this IEnumerable<T> source, Func<T, T, int> comparer)
+        {
+            T max = source.FirstOrDefault();
+            foreach (var item in source.Skip(1))
+            {
+                if (comparer(max, item) < 0)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+
+        public static T CustomMin<T>(this IEnumerable<T> source, Func<T, T, int> comparer)
+        {
+            T min = source.FirstOrDefault();
+            foreach (var item in source.Skip(1))
+            {
+                if (comparer(min, item) > 0)
+                {
+                    min = item;
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Extension Methods on IEnumerables/Extension Methods on IEnumerables/Program.cs
@@ -0,0 +1,29 @@
+﻿using Extension_Methods_on_IEnumerables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        // CustomAll
+        bool allEven = numbers.CustomAll(n => n % 2 == 0);
+        Console.WriteLine(allEven); // False
+
+        // CustomAny
+        bool anyOdd = numbers.CustomAny(n => n % 2 != 0);
+        Console.WriteLine(anyOdd); // True
+
+        // CustomMax
+        int maxNumber = numbers.CustomMax((a, b) => a.CompareTo(b));
+        Console.WriteLine(maxNumber); // 5
+
+        // CustomMin
+        int minNumber = numbers.CustomMin((a, b) => a.CompareTo(b));
+        Console.WriteLine(minNumber); // 1
+    }
+}
+
